43c973807fb6c862fc07e8d265b7750313646173
Rakshit Bhat <95375478+Raksbhat@users.noreply.github.com>
2024-01-29 21:33:32 +0530

    Update main.tf according to my region and ami code

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,15 +10,15 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-north-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-0014ce3e52359afbd"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "Terraform"
   }
 }
 
